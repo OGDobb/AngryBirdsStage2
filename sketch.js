6c92ac815f3a3ca54768715460e5199e0182d84d
OGDobb <awolfe0009@mymail.lausd.net>
2020-12-13 18:36:09 -0800

    platformadded

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    platform=new Ground(150,305,300,170);
     bird = new Bird(100,100);
 
 }
